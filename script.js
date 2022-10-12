1016f7d1e8b3fb1b81e7e2f78f74f7d6de21345a
Nelson <juniormoncada17@gmail.com>
2022-10-12 18:39:06 +0200

    menu desktop and menu mobile

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 const navbarEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const hamburgerMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 
 navbarEmail.addEventListener('click', toggleDesktopMenu);
 
-
 function toggleDesktopMenu(){
 
     desktopMenu.classList.toggle('inactive');
+}
+
+hamburgerMenu.addEventListener('click', toggleMobileMenu);
+
+function toggleMobileMenu(){
+
+    mobileMenu.classList.toggle('inactive');
 }
